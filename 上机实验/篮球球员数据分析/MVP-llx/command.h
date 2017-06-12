72195dc3b5aa07026c2435aafb1df4c2bae225e6
学生李令新 <stullx@qq.com>
2017-06-12 21:32:19 +0800

    读取指令，调用函数

--- 上机实验/篮球球员数据分析/MVP-llx/command.h
+++ 上机实验/篮球球员数据分析/MVP-llx/command.h
@@ -1,8 +1,9 @@
 #pragma once
 #pragma execution_character_set("utf-8")
+#include "diyOp.h"
 static short isFileReaded = 0;
 
-void command(void)
+int command(void)
 {
 	struct NODE * playerList = {0};
 	struct NODE * resList = { 0 };
@@ -10,17 +11,16 @@ void command(void)
 	do
 	{
 		printf ( "\n\n\t" );
-		printf ( "== == == == == == == == ==\n" );
-
-		printf ( "\t0-退出\t" );
-		printf ( "1-帮助\t" );
-		printf ( "2-读入数据" );
-		printf ( "\n\n" );
-		printf ( "\t3-选择排序" );
-
+		puts   ( "== == == == == == == == == == ==" );
+		printf( "\n" );
+		printf( "\t" );
+		puts( "0-退出      1-帮助      2-读入数据" ); printf( "\n\t" );
+		puts( "3-查看链表  4-选择排序  " ); 
 		printf ( "\n\t" );
-		printf ( "== == == == == == == == ==\n\n" );
-		printf ( "请输入指令\n" );
+		puts ( "== == == == == == == == == == ==" );
+		printf( "\n\n" );
+		puts ( "请输入指令" );
+		printf( "\n" );
 		scanf ( "%d", &cmd );
 
 		switch ( cmd )
@@ -29,24 +29,30 @@ void command(void)
 				help ();
 				break;
 			case 2:
-				playerList=contExec(fileRead ());
+				playerList=fileRead ();
+				if(playerList != 0)
+				{
+					puts( "数据已读入成功，并生成了单向链表【playerList】" );
+					printf( "\n" );
+				}
 				break;
 			case 3:
+				lookList(playerList);//查看链表
+				break;
+			case 4:
 				if(isFileReaded)
 					resList=selectSort (playerList);
 				else
 				{
-					printf( "\n您尚未输入数据，请输入后再执行该操作\n" );
+					printf( "\n" );
+					puts( "您尚未输入数据，请输入后再执行该操作" );
+					printf( "\n" );
 					break;
 				}
 
 			default:
-				;
-		}
-		if(playerList != 0)
-		{
-			printf( "数据已读入成功，并生成单向链表【playerList】\n" );
+				break;
 		}
 	} while ( cmd );
-	
+	return cmd;
 }
