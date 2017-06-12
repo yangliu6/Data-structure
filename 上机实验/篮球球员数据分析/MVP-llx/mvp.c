f1e5e0931c0850a7fb79c1f5c531547a460a2545
学生李令新 <stullx@qq.com>
2017-06-12 21:33:32 +0800

    主函数。

--- 上机实验/篮球球员数据分析/MVP-llx/mvp.c
+++ 上机实验/篮球球员数据分析/MVP-llx/mvp.c
@@ -26,6 +26,7 @@
 #include "help.h"
 #include "selectSort.h"
 #include "listAdd.h"
+#include "looklist.h"
 
 static short isSorted = 0;
 int main(void)
@@ -33,11 +34,15 @@ int main(void)
 	
 	//setlocale(LC_ALL,"Chinese_People's Republic of China.65001");//设定编码字符集utf-8的codepage=65001		无效。原因未知
 	system( "chcp 65001" );//设置编码,成功
+	//HANDLE handle_out;  //定义一个句柄  
+	//handle_out = GetStdHandle( STD_OUTPUT_HANDLE );   //获得标准输出设备句柄
+	//COORD size = { 80, 25 };      //定义一个坐标结构体
+	//SetConsoleScreenBufferSize( handle_out, size );   // 重新设置缓冲区大小
 	system( "cls" );
 	printf("\t== == == == == == == == ==\n");
 	printf("\t数据结构上机试验-综合实验四\n");
 	printf("\t== == == == == == == == ==\n\n");
 
-	command();
-	return 0;
+	
+	return command();;
 }
