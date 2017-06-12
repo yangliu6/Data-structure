8c3d23a580f64b9b757fab27d2f9c21fec655d2c
学生李令新 <stullx@qq.com>
2017-06-12 21:32:48 +0800

    该项目全体函数定义头文件

---
+++ 上机实验/篮球球员数据分析/MVP-llx/function.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+void substrR( char * , char * , long , long  );
+void help();
+struct NODE * contExec( char *  );
+struct NODE * fileRead( void );
+struct NODE * selectSort( struct NODE * );
+long kmp( char *, char * );
+void get_next( char* , int [] );
+void listAdd( struct NODE *, struct NODE *);
+int diyop( char * );
+void lookList( struct NODE *  );
+int command( void );
+int isChinese( char * );
